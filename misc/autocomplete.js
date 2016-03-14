5d6ca0ff81c806123dead475666e2f88e33eebbc
EHJ-52n <e.h.juerrens@52north.org>
2016-03-14 11:41:13 +0100

    Apply and improve ajax error message handling...
    in misc/ajax.js and misc/autocomple.js
    
    Log stuff only with error code >= 400 and better check for console

--- misc/autocomplete.js
+++ misc/autocomplete.js
@@ -310,7 +310,15 @@ Drupal.ACDB.prototype.search = function (searchString) {
         }
       },
       error: function (xmlhttp) {
-        alert(Drupal.ajaxError(xmlhttp, db.uri));
+        if (xmlhttp.status >= 400) {
+          if (window.console) {
+            console.log(Drupal.ajaxError(xmlhttp, db.uri));
+          }
+          else {
+            alert(Drupal.ajaxError(xmlhttp, db.uri));
+          }
+        }
+
       }
     });
   }, this.delay);
