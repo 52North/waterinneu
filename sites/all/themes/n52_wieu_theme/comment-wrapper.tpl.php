e7ccecb21d15bf648d0e453a6715cefed50305a7
EHJ-52n <e.h.juerrens@52north.org>
2016-04-21 16:58:31 +0200

    Update template: Comment display unobtrusive
    - implement own comment template using bootstrap accordion
    - add unobtrusive style options
    - fixes https://github.com/52North/waterinneu/issues/5

--- sites/all/themes/n52_wieu_theme/comment-wrapper.tpl.php
+++ sites/all/themes/n52_wieu_theme/comment-wrapper.tpl.php
@@ -26,31 +26,31 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
  */
- /*
-  * Changes to bootstrap business:
-  * - When rendering a tool node, we changes the Comment heading to a question 
-  */
- ?>
+/*
+ * Changes to bootstrap business:
+ * - We surround the comments and form with a accordion element and hide it by
+ *   default
+ */
+?>
 <div id="comments-accordion" class="panel-group">
 	<section id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 		<div class="panel panel-default panel-comments">
 			<div class="panel-heading collapsed" data-toggle="collapse" data-parent="#comments-accordion" data-target="#comments-panel">
 				<?php print render($title_prefix); ?>
-	    		<h2 class="title panel-title accordion-toggle"><?php print t('Comments'); ?> <span class="background">- (<?php print $node->comment_count;?>)</span></h2>
-	    		<?php print render($title_suffix); ?>
+				<h2 class="title panel-title accordion-toggle"><?php print t('Comments'); ?> <span class="background">- (<?php print $node->comment_count;?>)</span></h2>
+				<?php print render($title_suffix); ?>
 	    	</div>
 			<div id="comments-panel" class="panel-collapse collapse">
 				<div class="panel-body">
 					<?php if ($node->comment_count > 0) {?>
-						<h3><?php print t('What are the experiences users have had with the application of this product?'); ?></h3>
-	  					<?php print render($content['comments']); ?>
+						<?php print render($content['comments']); ?>
 	  				<?php } ?>
 					<?php if ($content['comment_form']){ ?>
-	    				<h3 class="title comment-form"><?php print t('What is your experience with this product?'); ?></h3>
+	    				<h3 class="title comment-form"><?php print t('Add your Comment'); ?></h3>
 	    				<?php print render($content['comment_form']); ?>
 	  				<?php } ?>
-   				</div>
-   			</div>
-   		</div>
+	   			</div>
+			</div>
+		</div>
 	</section>
 </div>
