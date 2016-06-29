367b3287d3e41432387770d073a74f9d2b6bc100
EHJ-52n <e.h.juerrens@52north.org>
2016-06-29 15:51:41 +0200

    Update feature "menus" and module "n52_default_nodes"
    - add links of footer menus
    - Update /cookies with useful content (fix for https://github.com/52North/waterinneu/issues/18)
    
    Fixes #18

--- sites/all/modules/n52_default_nodes/nodes/cookies.php
+++ sites/all/modules/n52_default_nodes/nodes/cookies.php
@@ -32,14 +32,138 @@ function _n52_default_nodes_cookies() {
 		'alias' => 'cookies',
 		'text' => array ('und' => '
 <p>
-   The Marketplace uses cookies for several reasons:
+	The Marketplace uses cookies for storing user preferences and session 
+	information for logged in users. By using it and agreeing to this policy, 
+	you consent to WaterInnEU\'s use of cookies in accordance with the terms of 
+	this policy.
+</p>
+<h2>About cookies</h2>
+<p>
+Cookies are files sent by web servers to web browsers, and stored by the web 
+browsers. The information is then sent back to the server each time the browser 
+requests a page from the server. This enables a web server to identify users 
+and apply the stored preferences.
+</p>
+<p>
+There are two main kinds of cookies:
 <ul>
-	<li><a href="#">#1: </a></li>
-	<li><a href="#">#2: </a></li>
-	<li><a href="#">#3: </a></li>
-	<li><a href="#">#4: </a></li>
-	<li><a href="#">#5: </a></li>
+	<li>session cookies and </li>
+	<li>persistent cookies.</li>
 </ul>
+Session cookies are deleted from your computer when you close your browser, 
+whereas persistent cookies remain stored on your computer until deleted, or 
+until they reach their expiry date.
+</p>
+<p>
+Find out more about <a href="http://www.aboutcookies.org/" target="_blank">how 
+to manage cookies</a>.
+<h2>Cookies on the marketplace</h2>
+<p>
+The WaterInnEU marketplace uses the following cookies for the listed purposes:
+<table>
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Duration<sup><a href="#note_star">*</a></sup></th>
+			<th>Description</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>has_js</td>
+			<td>Session</td>
+			<td>
+				Allows the server to know if it can depend upon javascript. Set to "1" 
+				if user has javascript enabled by whichever page a visitor arrives on.
+			</td>
+		</tr>
+		<tr>
+			<td>cookie-agreed</td>
+			<td>14 weeks</td>
+			<td>
+				Stores the information about the cookie acceptance of the user if are 
+				accepted.
+			</td>
+		</tr>
+		<tr>
+			<th colspan="3">When logged in</th>
+		</tr>
+		<tr>
+			<td>SESS***</td>
+			<td>3 weeks</td>
+			<td>
+				This cookie allows logging in and remembering which forms you have 
+				submitted. This cookie is essential for site functionality and the user 
+				experience.
+			</td>
+		</tr>
+		<tr>
+			<td>Drupal.***</td>
+			<td>3 weeks</td>
+			<td>
+				The Content Management System sets these cookies to store user 
+				interface preferences.
+			</td>
+		</tr>
+	</tbody>
+</table>
+</p>
+<h2>Third Party cookies</h2>
+<p>
+The WaterInnEU marketplace uses a widget from twitter.com. Hence, some cookies 
+from twitter are stored, too. These are the following ones:
+<table>
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Duration<a href="#note_star">*</a></th>
+			<th>Description</th>
+			<th>Domain/Provider</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>lang</td>
+			<td>Session</td>
+			<td>
+				Used to store the language of the user and to know which language to use
+				for the widget interface.
+			</td>
+			<td>syndication.twitter.com</td>
+		</tr>
+		<tr>
+			<td>metrics_token</td>
+			<td>4 weeks</td>
+			<td>Counts the number of users that have a specific embedded Tweet or timeline.</td>
+			<td>syndication.twitter.com</td>
+		</tr>
+		<tr>
+			<td>pid</td>
+			<td>end of time</td>
+			<td>
+				Enables Twitter\'s suggestions feature that personalizes content and 
+				suggestions for Twitter users based on their website visits.
+			</td>
+			<td>.twitter.com</td>
+		</tr>
+	</tbody>
+</table>
+</p>
+<h2>Refusing cookies</h2>
+<p>
+	Most browsers allow you to refuse to accept cookies. Here are the 
+	instructions for some browsers:
+<ul>
+	<li><a href="https://support.google.com/chrome/answer/95647?hl=en&p=cpn_cookies" target="_blank">Chrome</a></li>
+	<li><a href="https://support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences" target="_blank">Firefox</a></li>
+	<li><a href="https://support.microsoft.com/en-us/help/17442/windows-internet-explorer-delete-manage-cookies#ie=ie-11" target="_blank">Internet Explorer</a></li>
+	<li><a href="https://support.apple.com/en-us/HT201265" target="_blank">Safari</a></li>
+</ul>
+</p>
+<hr />
+<p>
+	<sup id="note_star"><strong>*</strong></sup> Any duration other then <code>Session</Code> denotes that this 
+	cookie is not deleted after ending the current browser session.
 </p>',),
 	);
 }
