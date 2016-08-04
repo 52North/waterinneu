71661dbace29f311faca469760ca1f17b168c63e
EHJ-52n <e.h.juerrens@52north.org>
2016-08-04 19:29:52 +0200

    Update module "n52_default_nodes": related-portals content

--- sites/all/modules/n52_default_nodes/nodes/related-portals.php
+++ sites/all/modules/n52_default_nodes/nodes/related-portals.php
@@ -259,120 +259,175 @@ function _n52_default_nodes_related_portals() {
 <div id="portal-list" class="container-fluid">
 <div class="row">
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/aquaknow_logo.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://www.aquaknow.net/" target="_blank"
 		title="aquaknow.net">Aquaknow</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				AquaKnow is a collaborative workspace and content management system 
+				dedicated to technical and scientific knowledge for the sustainable 
+				development of the water sector. It is a space for gathering and 
+				providing dynamic tools to manage scientific and technical information, 
+				including capacity building. It offers a specific space for developing 
+				collaborative work, based in Working groups. It is not a product 
+				oriented portal, but a contact point about the water management sector.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/cuahsihis-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://his.cuahsi.org/index.html" 
 			target="_blank">CUAHSI-HIS</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The CUAHSI Hydrologic Information System (HIS) is an internet-based 
+				system for sharing hydrologic data. It provides web services, tools, 
+				standards and procedures that enhance access to more and better data for
+				hydrologic analysis. The goal of the system is to offer access to and 
+				unite the USA nation\'s water information. No evaluations or additional 
+				supports are offered.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/datahub-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://datahub.io/">Datahub</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				DataHub is a free data management platform from the Open Knowledge 
+				Foundation, based on the CKAN data management system. CKAN is a tool for
+				managing and publishing collections of data. The Datahub provides free 
+				access to many of CKAN\'s core features, letting to search for data, 
+				create and manage groups of datasets, and get updates from datasets and 
+				groups. It is a general purpose portal, not specifically focused on 
+				water data or information.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/datacentereea-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://www.eea.europa.eu/themes/water/dc">EEA
 			Water Centre</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The Water Data Centre provides the European entry point for water 
+				related data as part of the Water Information System for Europe (WISE). 
+				It is a Data centre of the European Environment Agency that offers a 
+				catalogue of European datasets, interactive maps and indicators. It 
+				houses data and information collected by EU institutions to serve 
+				several stakeholders, although it doesn\'t provide tools or services. 
+				Contacts with institutions, networks or communities are neither offered.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/eionet-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://www.eionet.europa.eu/">EIONET</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The European environment information and observation network (Eionet) 
+				aims to provide timely and quality-assured data, information and 
+				expertise for assessing the state of the environment in Europe and the 
+				pressures acting upon it. Eionet is a partnership network of the 
+				European Environment Agency (EEA) and its member and cooperating 
+				countries. It provides a forum and access to data and tools, serving 
+				as a repository of the data uploaded by its members. It covers several 
+				environmental fields.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/the-european-innovation-partnership-on-water-online-marketplace-22-638.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://www.eip-water.eu/">EIP Water</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The EIP Water Market Place is a resource from the European Innovation 
+				Partnership (EIP) aimed at creating a grown community of practice, 
+				providing a single point of acces for information, being a matchmaking 
+				tool for sharing ideas and needs and providing a virtual meeting point 
+				for joint development in water innovation ideas. Users can freely 
+				register and edit a profile and join or consult an action group. It 
+				offers a water innovation map collecting news, organisations, projects 
+				and services. It is a good resource in creating community although not 
+				so efficient in finding really usable resources.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/euopendataportal-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="https://open-data.europa.eu/en/data"
 		target="_blank">EU Open Data Portal</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The European Union Open Data Portal is the single point of access to 
+				data from the institutions and other bodies of the European Union (EU). 
+				It aims to help foster the transparency and the accountability of the 
+				institutions and other bodies of the EU. Data is free to use and reuse 
+				for commercial or non-commercial purposes. To facilitate reuse, the 
+				portal provides a metadata catalogue based on common encoding rules and 
+				standardized vocabularies. A selection of applications built around EU 
+				data is also available. It is a general purpose data repository not 
+				specifically focused on water.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/jrcsciencehub-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://floods.jrc.ec.europa.eu/" target="_blank">European
 			Floods Portal</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The JRC Science Hub is the platform for information and exchange about 
+				all scientific activities carried out by the JRC and its institutes 
+				across Europe. It aims to gradually integrate and aggregate all of the 
+				European Commission\'s science related activities, tools, laboratories, 
+				facilities, databases and networks, enhancing the transparency and 
+				openness of the JRC and further enabling the open access policy of its 
+				scientific research data. It is restricted to the JRC research and not 
+				focused only to the water topic. It is organised in a directory 
+				structure with few search capabilities.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%//sites/default/files/logos/eurostat_logo-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://ec.europa.eu/eurostat" target="_blank">EUROSTAT
 			Open Data Portal</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The Eurostat\'s mission is to be the leading provider of high quality 
+				statistics on Europe. Eurostat\'s main role is to process and publish 
+				comparable statistical information at European level coming from the 
+				different Member States statistical authorities, by means of 
+				consolidating the data and ensure they are comparable, using harmonized 
+				methodology. In its portal statistical data and publications are offered
+				related to several topics in the EU.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/iwrmnet.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://europeanwatercommunity.eu/"
 		target="_blank">European Water Community (EWC)</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				IWRM-Net Scientific Coordination Project (IWRM-Net SCP) aims to provide 
+				the continuity of research projects’ follow up funded by IWRM-Net 1st 
+				and 2nd call both on administrative and technical sides. It offers 
+				information on the 6 funded projects and includes several webinars made 
+				within each one. It has no a broad approach as only covers few projects 
+				from the call and does not offer any kind of work community around them.
 		</p>
 	</div>
 	<div class="col-md-1">
@@ -389,63 +444,91 @@ function _n52_default_nodes_related_portals() {
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/inspire_logo.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://inspire-geoportal.ec.europa.eu/"
 		target="_blank">INSPIRE Geoportal</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The INSPIRE Directive requires the Commission to establish a community 
+				geo-portal and the Member States shall provide access to their 
+				infrastructures through the geo-portal. The INSPIRE geoportal provides 
+				the means to search for spatial data sets and spatial data services, 
+				and subject to access restrictions, to view spatial data sets from the 
+				EU Member States within the framework of the INSPIRE Directive. It\'s a 
+				general purpose portal offering access to a variety of sources of data, 
+				not specifically related to projects\' results in the water topic.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/switch-on-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://www.water-switch-on.eu/" 
 			target="_blank">SWITCH-ON</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The SWITCH-ON project provides free access to a number of 
+				water-information open data products and services. It offers information
+				on 14 tools, including contact details, a factsheet and a video demo for
+				each one. It also provides a map for searching data and a Virtual 
+				Water-Science Laboratory to conduct experiments with tools and data. 
+				SWITCH-ON promotes dialogues between water managers and other end users,
+				tool developers and scientists through a forum. It is focused on open 
+				data and not in standards and doesn\'t tackle the transferability of 
+				the products to the market or the end users opinions and validations on 
+				the products.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/waterwindow-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://waterwindow.org/" 
 			target="_blank">WaterWindow</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				WaterWindow is a catalog of solutions on the water sector. It offers a 
+				broad range of tools classified by topic, issue, spatial integration, 
+				water type, setting type, project period and degree of innovation. A 
+				brief description and the contact details are given for each tool. The 
+				portal is self-sustainable, as everybody is free to publish a solution 
+				after registering. No more information or support is given.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/wise-logo-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" href="http://water.europa.eu/" 
 			target="_blank">WISE</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The Water Information System for Europe (WISE) is defined as a gateway 
+				to information on European water issues. It comprises a wide range of 
+				data and information collected by EU institutions to serve several 
+				stakeholders: EU water policies, data and themes, modelling, and 
+				projects and research. It\'s an important source of information, but 
+				doesn\'t offer solutions or support to users in search for applied 
+				tools.
 		</p>
 	</div>
 	<div class="col-md-1">
-		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/logo.png" 
+		<img class="portal-logo" src="%BASE_URL%/sites/default/files/logos/wise-rtd-100.png" 
 			alt="Logo Example" width="100" height="100" />
 		<a class="portal-link" <a href="http://www.wise-rtd.info/en" 
 			target="_blank">WISE-RTD</a>
 		<p class="portal-description">
-			<strong>Expertise(s):</strong><br />
-			Nulla porttitor accumsan tincidunt. Vivamus magna justo, lacinia 
-			eget consectetur sed, convallis at tellus. Vestibulum ac diam sit 
-			amet quam vehicula elementum sed sit amet dui.
+				<strong>Expertise(s):</strong>
+				<br />
+				The WISE-RTD Water Knowledge Portal offers policy, research and 
+				industry resources related to water management. The information is 
+				structured in categories and linked by keywords. The portal forwards to 
+				websites containing a wide range of information with focus on 
+				information relevant for the implementation of the European Water 
+				Policy. No information about the applicability or support on the 
+				resources is given.
 		</p>
 	</div>
 </div>
