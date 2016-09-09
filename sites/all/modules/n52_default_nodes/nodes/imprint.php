8a26cafa0e137595acef87dbfebd0db8d7e84b42
EHJ-52n <e.h.juerrens@52north.org>
2016-09-09 10:41:11 +0200

    Update imprint: disclaimer, cookies reference

--- sites/all/modules/n52_default_nodes/nodes/imprint.php
+++ sites/all/modules/n52_default_nodes/nodes/imprint.php
@@ -35,20 +35,70 @@ function _n52_default_nodes_imprint() {
 		'alias' => 'imprint',
 		'text' => array (
 				'de' => '<p>
-<a href="http://52north.org/" target="_blank">52&deg;North GmbH</a> ist verantwortlich f&uuml;r diese Webseite.<br />
-52&deg;North Initiative for Geospatial Open Source Software GmbH<br />
+    52&deg;North Initiative for Geospatial Open Source Software GmbH<br />
 Martin-Luther-King-Weg 24<br />
-48155 M&uuml;nster, Deutschland
-</p>
-<p>&nbsp;</p>',
+48155 M&uuml;nster, Germany<br />
+<br />
+Telefon: +49 (0)251.396371-0<br />
+Telefax: +49 (0)251.396371-11<br />
+E-Mail: <a href="mailto:info@52north.org">info@52north.org</a><br />
+<a href="http://52north.org/">http://52north.org</a><br />
+<br />
+Gesch&auml;ftsf&uuml;hrer: Dr. Albert Remke, Dr. Andreas Wytzisk<br />
+<br />
+USt.-ID-Nr. DE253303206<br />
+<br />
+Handelsregister: Amtsgericht M&uuml;nster HRB 10849</p>
+<h2>Inhalt</h2>
+<p>Das Projektteam von enviroCar versucht mit bestem Wissen und Gewissen die 
+Inhalte dieser Website aktuell, vollständig und korrekt zu halten. Trotzdem 
+sind Fehler nicht auszuschließen. Das enviroCar Projekt übernimmt keinerlei 
+Verantwortung für Aktualität, Genauigkeit oder Vollständigkeit der auf dieser 
+Webseite dargestellen Inhalte, es sei denn der Fehler sei unter Absicht oder 
+aufgrund grober Fahrlässigkeit entstanden.</p>
+<p>WaterInnEU has no influence on the contents or reliability of the web sites 
+of third party information providers. The following therefore applies to all 
+links: WaterInnEU has no influence on the design and contents of third party 
+Internet web sites and therefore expressly distances itself from the material 
+of all third party Internet web sites. The declaration is valid for all links 
+given on our web site and the material of all sites reached via banners and 
+links on the WaterInnEU web site.</p>
+<h2>Cookies</h2>
+<p>Bitte konsultieren Sie die Seite <a href="cookies">&uuml;ber die Verwendung 
+von Cookies durch den WaterInnEU-Marktplatz-Prototypen</a>.</p>',
 				'en' => '
 <p>
-    <a href="http://52north.org/" target="_blank">52&deg;North GmbH</a> is 
-	responsible for this website.<br />
-	52&deg;North Initiative for Geospatial Open Source Software GmbH<br />
-	Martin-Luther-King-Weg 24<br />
-	48155 M&uuml;nster, Germany
-</p>
-<p>&nbsp;</p>',),
+    52&deg;North Initiative for Geospatial Open Source Software GmbH<br />
+Martin-Luther-King-Weg 24<br />
+48155 M&uuml;nster, Germany<br />
+<br />
+Phone: +49 (0)251.396371-0<br />
+Fax: +49 (0)251.396371-11<br />
+E-Mail: <a href="mailto:info@52north.org">info@52north.org</a><br />
+<a href="http://52north.org/">http://52north.org</a><br />
+<br />
+Managing Directors: Dr. Albert Remke, Dr. Andreas Wytzisk<br />
+<br />
+USt.-ID-Nr. DE253303206<br />
+<br />
+Commercial Registry: Amtsgericht M&uuml;nster HRB 10849</p>
+<h2>Content</h2>
+<p>The WaterInnEU project team makes every effort to ensure that the material 
+contained in its web site is current, complete and correct. Despite this, 
+errors cannot be completely ruled out. WaterInnEU does not accept liability 
+for the relevance, accuracy or completeness of the information and material 
+offered in its web site unless the mistake occurred intentionally or through 
+gross negligence. This refers to any loss, additional costs or damage of any 
+kind suffered as a result of any use of any material in this web site.</p>
+<p>WaterInnEU has no influence on the contents or reliability of the web sites 
+of third party information providers. The following therefore applies to all 
+links: WaterInnEU has no influence on the design and contents of third party 
+Internet web sites and therefore expressly distances itself from the material 
+of all third party Internet web sites. The declaration is valid for all links 
+given on our web site and the material of all sites reached via banners and 
+links on the WaterInnEU web site.</p>
+<h2>Cookies</h2>
+<p>Please take a look at the dedicated page about <a href="cookies">cookie 
+used by the WaterInnEU Marketplace Prototype</a>.</p>',),
 	);
 }
