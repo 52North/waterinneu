d274f2000f1e55049288638a7131ef639d7f1313
EHJ-52n <e.h.juerrens@52north.org>
2016-11-17 08:39:29 +0100

    Update feature "e-learning"
    - contrib module quiz 7.x-4.0-rc2 -> 7.x-4.0-rc3
    - update some translation files

--- sites/all/modules/quiz/question_types/quiz_h5p/quiz_h5p.js
+++ sites/all/modules/quiz/question_types/quiz_h5p/quiz_h5p.js
@@ -1,37 +1,52 @@
 (function ($) {
-  $(document).ready(function() {
+  $(document).ready(function () {
     if (H5P && H5P.externalDispatcher) {
       H5P.externalDispatcher.on('xAPI', function(event) {
-        // First try to get the score from the statement
-        var score = event.getScore();
-        var maxScore = event.getMaxScore();
-        
-        // If we didn't get it from the statement we get if from the H5P instance        
-        if (score === undefined || score === null) {
-          
-          // Get the H5P content id for the question
-          var contentId = event.getVerifiedStatementValue(['object', 'definition', 'extensions', 'http://h5p.org/x-api/h5p-local-content-id']);
-          
-          // If an H5P iframe exists we look there, if not it should be in our window
-          var instances = $('.h5p-iframe').length > 0 ? $('.h5p-iframe')[0].contentWindow.H5P.instances : H5P.instances;
-          
-          // Find the instance and get the score from it if possible
-          for (var i = 0; i < instances.length; i++) {
-            if (instances[i].contentId === contentId) {
-              if (typeof instances[i].getScore === 'function') {
-                score = instances[i].getScore();
-                maxScore = instances[i].getMaxScore();
-              }
-              break;
-            }
-          }
+        // try top level first
+        var instance = findGlobalInstance(getContentId(event));
+
+        // First try to get the score from the global instance
+        if (hasScoreData(instance)) {
+          updateScore(instance);
         }
-        if (score !== undefined && score !== null) {
-          var key = maxScore > 0 ? score / maxScore : 0;
-          key = (key + 32.17) * 1.234;
-          $('#quiz-h5p-result-key').val(key);
+        // Then try to get the score trough the statement
+        else if(hasScoreData(event)) {
+          updateScore(event);
         }
       });
     }
   });
+
+  function hasScoreData (obj){
+    return (
+      (typeof obj !== typeof undefined) &&
+      (typeof obj.getScore === 'function') &&
+      (typeof obj.getMaxScore === 'function')
+    );
+  }
+
+  function getContentId (event){
+    // Get the H5P content id for the question
+    return event.getVerifiedStatementValue(['object', 'definition', 'extensions', 'http://h5p.org/x-api/h5p-local-content-id']);
+  }
+
+  function findGlobalInstance (contentId){
+    var $iframes = $('.h5p-iframe');
+    var instances = $iframes.length > 0 ? $iframes[0].contentWindow.H5P.instances : H5P.instances;
+
+    return instances.find(function(instance){
+      return instance.contentId === contentId;
+    });
+  }
+
+  function updateScore (obj){
+    var score = obj.getScore(),
+      maxScore = obj.getMaxScore(),
+      key = (maxScore > 0) ? (score / maxScore) : 0;
+
+    // console.debug('update score:', score, ' of ', maxScore);
+
+    key = (key + 32.17) * 1.234;
+    $('#quiz-h5p-result-key').val(key);
+  }
 })(jQuery);
