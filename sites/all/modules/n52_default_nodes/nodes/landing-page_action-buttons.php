47fe306f3168a6a4d4362e4f67704090e99892d9
EHJ-52n <e.h.juerrens@52north.org>
2017-01-20 15:23:16 +0100

    Add new forum button to landing page buttons
    - add new button
    - move introduction to button element for better layout
    - remove introduction node
    - add styling for new buttons

--- sites/all/modules/n52_default_nodes/nodes/landing-page_action-buttons.php
+++ sites/all/modules/n52_default_nodes/nodes/landing-page_action-buttons.php
@@ -34,28 +34,36 @@ function _n52_default_nodes_landing_page_action_buttons() {
 		'alias' => NULL,
 		'text' => array ('en' => '
 				<div id="landing-page-action-buttons" class="container-fluid">
- 					<a class="col-lg-3 col-sm-6 col-xs-12" href="products-and-services">
+ 					<a class="col-lg-4 col-sm-6 col-xs-12" href="products-and-services">
 						<div id="landing-page-button-products-and-services" class="landing-page-button alert alert-info">
 							<div class="icon"><span class="glyphicon glyphicon-tasks"></span></div>
 							<div class="caption">products&nbsp;&amp;&nbsp;services</div>
 							<div class="explanation">Pre-screened tools for river basin management and matchmaking services between users and the supply chain</div>
 						</div>
 					</a>
-					<a class="col-lg-3 col-sm-6 col-xs-12" href="search/advanced">
+					<a class="col-lg-4 col-sm-6 col-xs-12" href="#about">
+						<div id="intro" class="landing-page-button alert alert-info">
+								The WaterInnEU Marketplace is a market led innovation platform
+								that screens the most relevant products and services for River 
+								Basin Managers and accelerates their uptake through targeted 
+								dissemination and support services.
+						</div>
+					</a>
+				<a class="col-lg-4 col-sm-6 col-xs-12" href="search/advanced">
 						<div id="landing-page-button-search" class="landing-page-button alert alert-info">
 							<div class="icon"><span class="glyphicon glyphicon-search"></span></div>
 							<div class="caption">search</div>
 							<div class="explanation">Find products, supply chain organisations, events, offers and requests for support</div>
 						</div>
 					</a>
-					<a class="col-lg-3 col-sm-6 col-xs-12" href="e-learning">
+					<a class="col-lg-4 col-sm-6 col-xs-12" href="e-learning">
 						<div id="landing-page-button-take-course" class="landing-page-button alert alert-info">
 							<div class="icon"><span class="glyphicon glyphicon-plus-sign"></span></div>
 							<div class="caption">take&nbsp;a&nbsp;course</div>
 							<div class="explanation">E-learning to help you implement specific products</div>
 						</div>
 					</a>
-					<a class="col-lg-3 col-sm-6 col-xs-12" href="join-us">
+					<a class="col-lg-4 col-sm-6 col-xs-12" href="join-us">
 						<div id="landing-page-button-join-us" class="landing-page-button alert alert-info"
 				data-content="<div class=&quot;popover-light&quot;>Already member? <a href=&quot;en/user/login?destination=join-us&quot;>Login <span class=&quot;glyphicon glyphicon-log-in&quot;></span></a></div>" 
 				data-container="body"
@@ -70,6 +78,13 @@ function _n52_default_nodes_landing_page_action_buttons() {
 							<div class="explanation">To communicate with other users: add your product or organisation, get support for commercialisation or implementation, and join the discussions!</div>
 						</div>
 					</a>
+				  <a class="col-lg-4 col-sm-6 col-xs-12" href="forum">
+						<div id="landing-page-button-forum" class="landing-page-button alert alert-info">
+							<div class="icon"><span class="glyphicon glyphicon-comment"></span></div>
+							<div class="caption">discus</div>
+							<div class="explanation">Participate in the latest debates on river basin management</div>
+						</div>
+				  </a>
 				</div>
 				',)
 			,
